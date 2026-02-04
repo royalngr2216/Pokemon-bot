c7ce9d38a85141ce62796baaf97a07d645b3184b
royalngr2216 <royalngr2216@gmail.com>
2026-02-04 23:56:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("Bot Online!");
+});
+
+client.login(process.env.TOKEN);
