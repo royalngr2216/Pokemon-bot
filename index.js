b8adee29329b2791e755f8221e29abc87ca5e3db
royalngr2216 <royalngr2216@gmail.com>
2026-02-05 00:07:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const data = JSON.parse(fs.readFileSync("./pokemon.json"));
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Bot Online!");
+
+  await client.application.commands.create({
+    name: "gengar",
+    description: "Show Gengar movesets"
+  });
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "gengar") {
+    const sets = data.gengar;
+
+    let reply = "**Gengar Sets:**\n\n";
+
+    sets.forEach(s => {
+      reply += `**${s.name}**\nItem: ${s.item}\nMoves: ${s.moves.join(", ")}\n\n`;
+    });
+
+    await interaction.reply(reply);
+  }
 });
 
 client.login(process.env.TOKEN);
