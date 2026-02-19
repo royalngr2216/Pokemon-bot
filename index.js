140d90b65be8e75deb10824c120d56ef6deded52
royalngr2216 <royalngr2216@gmail.com>
2026-02-20 01:15:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is alive"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Web server ready"));
+
 const {
   Client,
   GatewayIntentBits,
