374915ff2f880e81f54ee83536ba4feb4e75eb1a
royalngr2216 <royalngr2216@gmail.com>
2026-05-08 05:01:17 +0530

    Create mongoose.js

---
+++ database/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose =
+  require("mongoose");
+
+module.exports = async () => {
+
+  try {
+
+    await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      "✅ MongoDB Connected"
+    );
+
+  } catch (err) {
+
+    console.error(
+      "❌ MongoDB Error:",
+      err
+    );
+  }
+};
