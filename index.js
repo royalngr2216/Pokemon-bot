fd07384407e07a7192bedba8af58a6c810e0b869
royalngr2216 <royalngr2216@gmail.com>
2026-05-08 05:02:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Bot is alive"));
-app.listen(process.env.PORT || 3000);
+app.get("/", (req, res) =>
+  res.send("Bot is alive")
+);
+
+app.listen(
+  process.env.PORT || 3000
+);
 
 const {
   Client,
@@ -12,49 +18,115 @@ const {
 
 const fs = require("fs");
 
+// =========================
+// MONGODB
+// =========================
+
+const connectMongo =
+  require("./database/mongoose");
+
+// =========================
+// CLIENT
+// =========================
+
 const client = new Client({
+
   intents: [
+
     GatewayIntentBits.Guilds,
+
     GatewayIntentBits.DirectMessages
+
   ]
 });
 
-client.commands = new Collection();
+client.commands =
+  new Collection();
 
+// =========================
 // LOAD COMMANDS
+// =========================
+
 const commandFiles = fs
+
   .readdirSync("./commands")
-  .filter(file => file.endsWith(".js"));
+
+  .filter(file =>
+    file.endsWith(".js")
+  );
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
+
+  const command =
+    require(`./commands/${file}`);
+
+  client.commands.set(
+    command.data.name,
+    command
+  );
 }
 
+// =========================
 // LOAD EVENTS
+// =========================
+
 const eventFiles = fs
+
   .readdirSync("./events")
-  .filter(file => file.endsWith(".js"));
+
+  .filter(file =>
+    file.endsWith(".js")
+  );
 
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
+
+  const event =
+    require(`./events/${file}`);
+
   event(client);
 }
 
+// =========================
 // READY
-client.once("clientReady", async () => {
-  console.log("Bot Online");
+// =========================
 
-  const commands = [];
+client.once(
+  "clientReady",
 
-  client.commands.forEach(cmd => {
-    commands.push(cmd.data.toJSON());
-  });
+  async () => {
 
-  await client.application.commands.set(commands);
+    console.log(
+      "Bot Online"
+    );
 
-  console.log("Slash commands loaded");
-});
+    const commands = [];
 
+    client.commands.forEach(cmd => {
+
+      commands.push(
+        cmd.data.toJSON()
+      );
+    });
+
+    await client.application
+      .commands.set(commands);
+
+    console.log(
+      "Slash commands loaded"
+    );
+  }
+);
+
+// =========================
+// CONNECT MONGODB
+// =========================
+
+connectMongo();
+
+// =========================
 // LOGIN
-client.login(process.env.TOKEN);
+// =========================
+
+client.login(
+  process.env.TOKEN
+);
