6a5d11a7c5f1f6e771d4bbab5122bd03406c30b0
royalngr2216 <royalngr2216@gmail.com>
2026-05-08 03:26:50 +0530

    Add timezones data to utils/timezones.js

---
+++ utils/timezones.js
@@ -0,0 +1,237 @@
+module.exports = [
+
+  // GMT -12
+  {
+    label: "GMT-12:00 — Baker Island",
+    value: "Etc/GMT+12"
+  },
+
+  // GMT -11
+  {
+    label: "GMT-11:00 — American Samoa",
+    value: "Pacific/Pago_Pago"
+  },
+
+  // GMT -10
+  {
+    label: "GMT-10:00 — Hawaii",
+    value: "Pacific/Honolulu"
+  },
+
+  // GMT -9:30
+  {
+    label: "GMT-09:30 — Marquesas",
+    value: "Pacific/Marquesas"
+  },
+
+  // GMT -9
+  {
+    label: "GMT-09:00 — Alaska",
+    value: "America/Anchorage"
+  },
+
+  // GMT -8
+  {
+    label: "GMT-08:00 — Pacific Time",
+    value: "America/Los_Angeles"
+  },
+
+  // GMT -7
+  {
+    label: "GMT-07:00 — Mountain Time",
+    value: "America/Denver"
+  },
+
+  // GMT -6
+  {
+    label: "GMT-06:00 — Central Time",
+    value: "America/Chicago"
+  },
+
+  // GMT -5
+  {
+    label: "GMT-05:00 — Eastern Time",
+    value: "America/New_York"
+  },
+
+  // GMT -4:30
+  {
+    label: "GMT-04:30 — Venezuela",
+    value: "America/Caracas"
+  },
+
+  // GMT -4
+  {
+    label: "GMT-04:00 — Atlantic Time",
+    value: "America/Halifax"
+  },
+
+  // GMT -3:30
+  {
+    label: "GMT-03:30 — Newfoundland",
+    value: "America/St_Johns"
+  },
+
+  // GMT -3
+  {
+    label: "GMT-03:00 — Argentina",
+    value: "America/Argentina/Buenos_Aires"
+  },
+
+  // GMT -2
+  {
+    label: "GMT-02:00 — South Georgia",
+    value: "Atlantic/South_Georgia"
+  },
+
+  // GMT -1
+  {
+    label: "GMT-01:00 — Azores",
+    value: "Atlantic/Azores"
+  },
+
+  // GMT 0
+  {
+    label: "GMT+00:00 — London",
+    value: "Europe/London"
+  },
+
+  // GMT +1
+  {
+    label: "GMT+01:00 — Berlin",
+    value: "Europe/Berlin"
+  },
+
+  // GMT +2
+  {
+    label: "GMT+02:00 — Cairo",
+    value: "Africa/Cairo"
+  },
+
+  // GMT +3
+  {
+    label: "GMT+03:00 — Moscow",
+    value: "Europe/Moscow"
+  },
+
+  // GMT +3:30
+  {
+    label: "GMT+03:30 — Iran",
+    value: "Asia/Tehran"
+  },
+
+  // GMT +4
+  {
+    label: "GMT+04:00 — Dubai",
+    value: "Asia/Dubai"
+  },
+
+  // GMT +4:30
+  {
+    label: "GMT+04:30 — Afghanistan",
+    value: "Asia/Kabul"
+  },
+
+  // GMT +5
+  {
+    label: "GMT+05:00 — Pakistan",
+    value: "Asia/Karachi"
+  },
+
+  // GMT +5:30
+  {
+    label: "GMT+05:30 — India",
+    value: "Asia/Kolkata"
+  },
+
+  // GMT +5:45
+  {
+    label: "GMT+05:45 — Nepal",
+    value: "Asia/Kathmandu"
+  },
+
+  // GMT +6
+  {
+    label: "GMT+06:00 — Bangladesh",
+    value: "Asia/Dhaka"
+  },
+
+  // GMT +6:30
+  {
+    label: "GMT+06:30 — Myanmar",
+    value: "Asia/Yangon"
+  },
+
+  // GMT +7
+  {
+    label: "GMT+07:00 — Thailand",
+    value: "Asia/Bangkok"
+  },
+
+  // GMT +8
+  {
+    label: "GMT+08:00 — China",
+    value: "Asia/Shanghai"
+  },
+
+  // GMT +8:45
+  {
+    label: "GMT+08:45 — Australia (Eucla)",
+    value: "Australia/Eucla"
+  },
+
+  // GMT +9
+  {
+    label: "GMT+09:00 — Japan",
+    value: "Asia/Tokyo"
+  },
+
+  // GMT +9:30
+  {
+    label: "GMT+09:30 — Australia (Darwin)",
+    value: "Australia/Darwin"
+  },
+
+  // GMT +10
+  {
+    label: "GMT+10:00 — Sydney",
+    value: "Australia/Sydney"
+  },
+
+  // GMT +10:30
+  {
+    label: "GMT+10:30 — Australia (Lord Howe)",
+    value: "Australia/Lord_Howe"
+  },
+
+  // GMT +11
+  {
+    label: "GMT+11:00 — Solomon Islands",
+    value: "Pacific/Guadalcanal"
+  },
+
+  // GMT +12
+  {
+    label: "GMT+12:00 — New Zealand",
+    value: "Pacific/Auckland"
+  },
+
+  // GMT +12:45
+  {
+    label: "GMT+12:45 — Chatham Islands",
+    value: "Pacific/Chatham"
+  },
+
+  // GMT +13
+  {
+    label: "GMT+13:00 — Samoa",
+    value: "Pacific/Apia"
+  },
+
+  // GMT +14
+  {
+    label: "GMT+14:00 — Line Islands",
+    value: "Pacific/Kiritimati"
+  }
+
+];
