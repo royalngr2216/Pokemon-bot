cb5ac0e75f91bcdf6e61f7cdbca625eba704a3d3
royalngr2216 <royalngr2216@gmail.com>
2026-05-08 06:12:46 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,40 +18,46 @@ module.exports = {
     const embed =
       new EmbedBuilder()
 
+        .setColor(0x2B2D31)
+
         .setTitle(
           "ORAS UTILITIES"
         )
 
-        .setColor(
-          0x5865F2
-        )
-
         .setDescription(
 
-`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+`\`\`\`ansi
+[2;35m━━━━━━━━━━ ⚔ COMPETITIVE ━━━━━━━━━━[0m
+\`\`\`
 
-━━━━━━━━ ⚔ COMPETITIVE ━━━━━━━━
+\`\`\`ansi
+[2;37mCommand :[0m [2;32m/pokemon[0m
+[2;37mAction  :[0m [2;32mView Pokémon sets and resources[0m
+\`\`\`
 
-Command : /pokemon
-Action  : View Pokémon sets and resources
+\`\`\`ansi
+[2;36m━━━━━━━━━━ 🌍 UTILITIES ━━━━━━━━━━━[0m
+\`\`\`
 
-━━━━━━━━ 🌍 UTILITIES ━━━━━━━━━
+\`\`\`ansi
+[2;37mCommand :[0m [2;32m/tz[0m
+[2;37mAction  :[0m [2;32mCreate localized tournament times[0m
 
-Command : /tz
-Action  : Create localized tournament times
+[2;37mCommand :[0m [2;32m/remind[0m
+[2;37mAction  :[0m [2;32mTournament reminders[0m
+\`\`\`
 
-Command : /remind
-Action  : Tournament reminders
+\`\`\`ansi
+[2;33m━━━━━━━━━━ 🏆 TOURNAMENT ━━━━━━━━━━[0m
+\`\`\`
 
-━━━━━━━━ 🏆 TOURNAMENT ━━━━━━━━
+\`\`\`ansi
+[2;37mCommand :[0m [2;32m/schedule[0m
+[2;37mAction  :[0m [2;32mSchedule tournament matches[0m
 
-Command : /schedule
-Action  : Schedule tournament matches
-
-Command : /matches
-Action  : View scheduled matches
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+[2;37mCommand :[0m [2;32m/matches[0m
+[2;37mAction  :[0m [2;32mView scheduled matches[0m
+\`\`\``
 
         );
 
