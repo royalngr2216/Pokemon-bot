64f3aeeebfd14973ab7ec3d878a40d756f736b63
royalngr2216 <royalngr2216@gmail.com>
2026-05-12 04:04:56 +0530

    Update tourReminders.js

--- systems/tourReminders.js
+++ systems/tourReminders.js
@@ -1,3 +1,7 @@
+const {
+  EmbedBuilder
+} = require("discord.js");
+
 const Tour =
   require("../models/Tour");
 
@@ -29,7 +33,7 @@ module.exports = client => {
           tour.scheduledFor - now;
 
         // =========================
-        // 15 MINUTES
+        // 15 MINUTES BEFORE
         // =========================
 
         if (
@@ -46,18 +50,65 @@ module.exports = client => {
                 tour.discordId
               );
 
-            await user.send(
+            const colors = [
 
-              `⏰ Your match vs **${tour.opponent}** starts in 15 minutes!\n\n` +
+              0x5865F2,
+              0x57F287,
+              0xFEE75C,
+              0xEB459E,
+              0xED4245,
+              0x3498DB,
+              0x9B59B6
 
-              `🕒 <t:${Math.floor(
-                tour.scheduledFor / 1000
-              )}:F>\n\n` +
+            ];
 
-              `⏳ <t:${Math.floor(
-                tour.scheduledFor / 1000
-              )}:R>`
-            );
+            const randomColor =
+              colors[
+                Math.floor(
+                  Math.random() *
+                  colors.length
+                )
+              ];
+
+            const embed =
+              new EmbedBuilder()
+
+                .setTitle(
+                  "🏆 Tournament Reminder"
+                )
+
+                .setColor(
+                  randomColor
+                )
+
+                .setDescription(
+
+                  `You have to play against **${tour.opponent}** in 15 minutes.\n\n` +
+
+                  `🕒 **Match Time**\n` +
+
+                  `<t:${Math.floor(
+                    tour.scheduledFor / 1000
+                  )}:F>\n\n` +
+
+                  `⏳ **Starts**\n` +
+
+                  `<t:${Math.floor(
+                    tour.scheduledFor / 1000
+                  )}:R>`
+                )
+
+                .setFooter({
+
+                  text:
+                    "Good luck & have fun ⚔️"
+                })
+
+                .setTimestamp();
+
+            await user.send({
+              embeds: [embed]
+            });
 
             tour.reminded =
               true;
